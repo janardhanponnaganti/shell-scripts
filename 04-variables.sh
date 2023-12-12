d1486713ce0b2e093b7faf4920e984cb5abaad49
janardhanponnaganti <ponnagantijr@gmail.com>
2023-12-12 23:25:52 +0100

    person1

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/blash
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: how are $PERSON2"
+echo "$PERSON2: im good $PERSON1"
+echo "$PERSON1: happy new year $PERS0N2"
+echo "$PERSON2:thank you $ram"
+echo " sampamth"
