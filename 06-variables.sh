e3b2eeb757273bdb091e91079b4ff553f52409bc
janardhanponnaganti <ponnagantijr@gmail.com>
2023-12-13 00:04:46 +0100

    person1

---
+++ 06-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter your username :"
+
+read -s USERNAME #the value entered above will be automatically atteched USERNAME variable
+
+echo "please enter your passwood :"
+read -s PASSWORD
+
+echo "username is : $USERNAME, password is : $PASSWORD" # i am printing just for validation you should not print username and password
