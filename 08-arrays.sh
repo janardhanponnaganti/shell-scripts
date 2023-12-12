a8a80ff4e0d4b17d009f832d7dad564489d9ebbb
janardhanponnaganti <ponnagantijr@gmail.com>
2023-12-13 00:59:11 +0100

    person1

---
+++ 08-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+FRUITS=("apple" "banana" "mango")
+
+echo "frist value: ${FRUITS[0]}
+
+echo "second value: ${FRUITS[1]}"
+
+echo "third value: ${FRUITS[2]}"
+
+echo "all fruits: ${FRUITS[2]}
