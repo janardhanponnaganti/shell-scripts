233f532aae5460bda5fe36a35b617901822b46db
janardhanponnaganti <ponnagantijr@gmail.com>
2023-12-14 23:21:50 +0100

    mysql install

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,8 +5,17 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then
     echo "ERROR:: plese run this script with root access"
-else 
+    exit 1 # you can give other then 0
+else
     echo "you are root user"
-fi
+fi # fi means reverse of if, indicating condition end
 
 yum install mysql -y
+
+if [ $? -ne 0 ]
+then 
+    echo "error:: installing mysql is failed"
+    exit 1
+else
+    echo "installing Mysql is success"
+fi
